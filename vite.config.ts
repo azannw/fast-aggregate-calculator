69ff7a60ab5fdb1c584a78eaa0ef0e8b723a4fbf
azan <itsazanwaseem@gmail.com>
2025-04-17 00:55:12 +0500

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: "/fast-aggregate-calculator",
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
