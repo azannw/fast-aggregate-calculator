aa11c6d0d81d69d298a6243e64a655ce351c8544
Azan <datbydat@gmail.com>
2025-04-21 22:16:37 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
+// vite.config.ts
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/', // very important for Vercel
   plugins: [react()],
-  base: "/fast-aggregate-calculator",
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
 });
